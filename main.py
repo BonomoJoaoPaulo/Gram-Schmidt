3a07b07667f3314ffcc69b02288ddf349ee8896c
BonomoJoaoPaulo <jpabonomo@gmail.com>
2022-06-22 01:43:44 -0300

    Internal Product function

--- main.py
+++ main.py
@@ -9,5 +9,23 @@ for i in range(0, dimension):
 
 base = tuple(vectors_list)
 
+"""
+def gram_schmidt(vector_conj):
+    ortogonal_base = []
+    ortogonal_base[0] = vector_conj[0]
+    for i in range(1, len(vector_conj)+1):
+        ortogonal_base[i] = vector_conj[i] - (internal_product(vector_conj[i], vector_conj[i-1])/internal_product(vector_conj[i-1], vector_conj[i-1])) * vector_conj[i-1]
+    return ortogonal_base
+"""
+
+def internal_product(v, u):
+    ip = 0
+    for a in range(0, len(v)):
+        ip += v[a] * u[a]
+    
+    return ip
+
+
 print(base)
-time.sleep(5)
+print(internal_product(base[1], base[1]))
+time.sleep(10)
